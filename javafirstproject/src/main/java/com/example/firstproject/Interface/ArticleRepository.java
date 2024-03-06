21ccd1bc941538374133e0829d63b381682b62ee
Hyunjun-Ko <85072422+Hyunjun-Ko@users.noreply.github.com>
2024-03-06 22:38:31 +0900

    5.2 reading data from db and showing on view page
    1. created an index() in ArticleController which this time "findAll()" of
    data in db and return an Iterable.
    2. override findAll to return an ArrayList instead of an Iterable.
    3. created a view page index.mustache a clone to show.mustache but iterates
    to show all data instead of single article.

--- javafirstproject/src/main/java/com/example/firstproject/Interface/ArticleRepository.java
+++ javafirstproject/src/main/java/com/example/firstproject/Interface/ArticleRepository.java
@@ -2,7 +2,11 @@ package com.example.firstproject.Interface;
 
 import com.example.firstproject.entity.Article;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.ArrayList;
 // CrudRepository 패키지 자동 임포트
 
 public interface ArticleRepository extends CrudRepository<Article, Long> {
+    @Override
+    ArrayList<Article> findAll(); // Iterable -> ArrayList 수정
 }
