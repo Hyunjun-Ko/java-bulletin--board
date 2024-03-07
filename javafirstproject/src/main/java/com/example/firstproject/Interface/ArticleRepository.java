27739fa7cc09ea5ea1de5013f67b1f69bf938856
Hyunjun-Ko <85072422+Hyunjun-Ko@users.noreply.github.com>
2024-03-07 22:06:53 +0900

    7.1 Updating data from db(created an edit page, bring data from db)
    1. created an edit button to go to "article/{id}/edit" and
    also a back button to go back to "article/{id}"
    2. controller then finds data from db by id.
    3. controller then adds to the model
    4. the data in the model is then shown on "article/{id}/edit"

--- javafirstproject/src/main/java/com/example/firstproject/Interface/ArticleRepository.java
+++ javafirstproject/src/main/java/com/example/firstproject/Interface/ArticleRepository.java
@@ -2,9 +2,9 @@ package com.example.firstproject.Interface;
 
 import com.example.firstproject.entity.Article;
 import org.springframework.data.repository.CrudRepository;
-
-import java.util.ArrayList;
 // CrudRepository 패키지 자동 임포트
+import java.util.ArrayList;
+
 
 public interface ArticleRepository extends CrudRepository<Article, Long> {
     @Override
