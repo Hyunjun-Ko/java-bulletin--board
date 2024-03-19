f5490202671bb0d2be6720f1992eec631ee699e4
Hyunjun-Ko <85072422+Hyunjun-Ko@users.noreply.github.com>
2024-03-19 20:49:00 +0900

    12.1 implementing service layer to work with controller and repository
    instead of having the controller direct client requests and access DB,
    the controller now only controls and the ArticleService implemented now
    takes care of repository and service logic.

--- javafirstproject/src/main/java/com/example/firstproject/api/ArticleApiController.java
+++ javafirstproject/src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -1,14 +1,13 @@
 package com.example.firstproject.api;
 
-import com.example.firstproject.Interface.ArticleRepository;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
+import com.example.firstproject.service.ArticleService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.service.annotation.PatchExchange;
 
 import java.util.List;
 
@@ -16,58 +15,45 @@ import java.util.List;
 @RestController // REST 컨트롤러 선언
 public class ArticleApiController {
     @Autowired // 게시글 리파지터리 주입
-    private ArticleRepository articleRepository;
+    private ArticleService articleService;
 
     // GET
     @GetMapping("/api/articles") // URL 요청 접수
     public List<Article> index() { // index() 메서드 정의
-        return articleRepository.findAll();
+        return articleService.index();
     }
 
     @GetMapping("/api/articles/{id}") // 코드를 붙여 넣고 URL 수정
     public Article show(@PathVariable Long id) { // show() 메서드로 수정
-        return articleRepository.findById(id).orElse(null);
+        return articleService.show(id);
     }
 
     // POST
     @PostMapping("/api/articles")
-    public Article create(@RequestBody ArticleForm dto) {
-        Article article = dto.toEntity();
-        return articleRepository.save(article);
+    public ResponseEntity<Article> create(@RequestBody ArticleForm dto) {
+        Article created = articleService.create(dto);
+        return (created != null) ? // 생성하면 정상, 실패하면 오류 응답
+                ResponseEntity.status(HttpStatus.OK).body(created) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+
     }
 
     // PATCH
     @PatchMapping("/api/articles/{id}")
     public ResponseEntity<Article> update(@PathVariable Long id,
                                           @RequestBody ArticleForm dto) {
-        // 1. DTO -> 엔티티 변환하기
-        Article article = dto.toEntity();
-        log.info("id: {}, article: {}", id, article.toString());
-        // 2. 타깃 조회하기
-        Article target = articleRepository.findById(id).orElse(null);
-        // 3. 잘못된 요청 처리하기
-        if (target == null || id != article.getId()) {
-            // 400, 잘못된 요청 응답!
-            log.info("잘못된 요청! id: {}, article: {}", id, article.toString());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-        // 4. 업데이트 및 정상 응답(200)하기
-        target.patch(article);
-        Article updated = articleRepository.save(target);
-        return ResponseEntity.status(HttpStatus.OK).body(updated);
+        Article updated = articleService.update(id, dto);
+        return (updated != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(null) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build(); // build() = body(null)
     }
 
     // DELETE
     @DeleteMapping("/api/articles/{id}")
     public ResponseEntity<Article> delete(@PathVariable Long id) {
-        // 1. 대상 찾기
-        Article target = articleRepository.findById(id).orElse(null);
-        // 2. 잘못된 요청 처리하기
-        if (target == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-        // 3. 대상 삭제하기
-        articleRepository.delete(target);
-        return ResponseEntity.status(HttpStatus.OK).build(); // build() = body(null)
+        Article deleted = articleService.delete(id);
+        return (deleted != null) ?
+                ResponseEntity.status(HttpStatus.NO_CONTENT).build() :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 }
