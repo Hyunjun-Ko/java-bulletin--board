547707b9581c8e64bc0fcd18273c8da2ac71672a
Hyunjun-Ko <85072422+Hyunjun-Ko@users.noreply.github.com>
2024-03-27 21:51:06 +0900

    16. created a view page to show comments in articles.
    1. modified the mvc pattern ArticleController to now fetch comments by
    article id and show them via updating the model then to the viewpage.

--- javafirstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ javafirstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -2,7 +2,10 @@ package com.example.firstproject.controller;
 
 import com.example.firstproject.Interface.ArticleRepository;
 import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.dto.CommentDto;
 import com.example.firstproject.entity.Article;
+import com.example.firstproject.entity.Comment;
+import com.example.firstproject.service.CommentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,12 +16,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j // 로깅 기능을 위한 어노테이션 추가, println() 대체
 @Controller
 public class ArticleController {
     @Autowired // 스프링 부트가 미리 생성해 놓은 리파지터리 객체 주입(DI)
     private ArticleRepository articleRepository; // articleRepository 객체 선언
+
+    @Autowired
+    private CommentService commentService; // 서비스 객체 주입
 
     @GetMapping("/articles/new")
     public String newArticleForm() {
@@ -42,8 +49,10 @@ public class ArticleController {
         log.info("id = " + id); // id를 잘 받았는지 로그 찍기
         // 1. id를 조회해 데이터 가져오기
         Article articleEntity = articleRepository.findById(id).orElse(null);
+        List<CommentDto> commentsDtos = commentService.comments(id);
         // 2. 모델에 데이터 등록하기
         model.addAttribute("article", articleEntity);
+        model.addAttribute("commentDtos", commentsDtos);
         // 3. 뷰 페이지 반환하기
         return "articles/show"; // 목록으로 돌아가기 링크를 넣을 뷰 파일 확인
     }
